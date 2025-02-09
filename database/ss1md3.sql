657ab704f755b9aa425345abc790b7598d039e0e
HungLe <hung0964153699@gmail.com>
2025-02-09 11:36:10 +0700

    ss1_2

--- database/ss1md3.sql
+++ database/ss1md3.sql
@@ -1,10 +1,10 @@
 create database if not exists cl09md3;
 use cl09md3;
-create table if not exists Class(
+create table if not exists class(
   id int primary key auto_increment,
   `name` varchar(100) not null default ("abc")
 );
-create table if not exists Teachers(
+create table if not exists teachers(
   id int primary key auto_increment,
   `name` varchar(100) not null default ("abc"),
   `age` int(100) not null default(0),
