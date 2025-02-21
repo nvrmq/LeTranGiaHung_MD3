594ea6a3fa67079b1d2edd87445e1eef11673eb4
HungLe <hung0964153699@gmail.com>
2025-02-21 19:05:43 +0700

    case_study2

--- database/case_study/1den5.sql
+++ database/case_study/1den5.sql
@@ -213,3 +213,27 @@ group by kh.ma_khach_hang, kh.ho_ten
 order by so_lan_dat_phong;
 
 -- cau 5
+select 
+	kh.ma_khach_hang,
+    kh.ho_ten,
+    lk.ten_loai_khach,
+    hd.ma_hop_dong,
+    dv.ten_dich_vu,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    coalesce(sum(hdct.so_luong * dvdk.gia), 0) + coalesce(sum(dv.chi_phi_thue), 0) as tong_tien
+from khach_hang kh
+left join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+left join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang
+left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+left join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+left join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu
+group by
+	kh.ma_khach_hang, 
+    kh.ho_ten, 
+    lk.ten_loai_khach, 
+    hd.ma_hop_dong, 
+    dv.ten_dich_vu, 
+    hd.ngay_lam_hop_dong, 
+    hd.ngay_ket_thuc
+order by kh.ma_khach_hang, hd.ma_hop_dong;
